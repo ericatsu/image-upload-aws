a33b73f6233abaadf30de3646953866c443aaf82
ericatsu <ekaatsu@st.ug.edu.gh>
2023-05-16 20:06:33 +0000

    🚑

---
+++ src/main/java/com/lxidus/imageuploadaws/ImageUploadAwsApplication.java
@@ -0,0 +1,13 @@
+package com.lxidus.imageuploadaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUploadAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageUploadAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lxidus/imageuploadaws/ImageUploadAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxidus.imageuploadaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageUploadAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
